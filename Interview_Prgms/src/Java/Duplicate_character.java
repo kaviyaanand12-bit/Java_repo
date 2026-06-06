48b7cd27b4f22c5907d840dd6ef4e6da1b8de43d
Kaviya Anand <kaviyaanand30122000@gmail.com>
2026-06-06 23:59:17 +0530

    thirdone

---
+++ Interview_Prgms/src/Java/Duplicate_character.java
@@ -0,0 +1,11 @@
+package Java;
+
+public class Duplicate_character {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//duplicate character in a string
+	}
+
+}
